048b32385de5895e8e56d0de134f44759891394a
Tudor Zaharia <zahariatm@yahoo.com>
2024-11-19 14:50:05 +0200

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main() {
+    std::cout << "TUDORRR\n";
+
+    int a;
+    int b;
+
+    std::cin >> a >> b;
+
+    std::cout << "a = " << a << ", b = " << b << "\n";
+    std::cout << "Sum is: " << a + b << "\n";
+
+    return 0;
+}
